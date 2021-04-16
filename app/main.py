c15b07e901cdb193fa0b22435da21d5e0b62251f
AhmedQuas <ahmed.quas@gmail.com>
2021-04-16 16:30:21 +0200

    Python FastAPI basic setup & endpoint declaration

--- app/main.py
+++ app/main.py
@@ -3,6 +3,15 @@ from fastapi import FastAPI
 #API documentation is accessible by /doc or /redoc path
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'status':'ok'}
+@app.get('/statistics')
+def show_statistics():
+    return {'status':'statistics ok'}
+
+@app.post('/survey')
+def survey():
+    return {'status':'survey ok'}
+
+
+@app.post('/test-data')
+def test_data():
+    return {'status': 'test-data ok'}
